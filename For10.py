d5156ade8ca5fa513c43978d9f5eebcd25d0447a
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-11-06 23:16:01 +0500

    For10.py

--- For10.py
+++ For10.py
@@ -6,4 +6,10 @@ def main(list1):
     Returns:
         list: return  answer
     """
-    return
+    s=[]
+    for i in list1:
+        if str(i).islower():
+            s.append(str(i).capitalize())
+    
+    return s
+print(main(['kamronbek','javoxir','diyor','ikrom']))
